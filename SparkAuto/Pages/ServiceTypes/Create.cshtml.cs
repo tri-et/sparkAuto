25911a1212ff0fc535d3e4f82ee1a2cb0b1691ce
john <tripham178@gmail.com>
2020-04-12 22:24:25 +0700

    first commit

---
+++ SparkAuto/Pages/ServiceTypes/Create.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public ServiceType ServiceType { get; set; }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync(ServiceType serviceType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _db.ServiceType.Add(serviceType);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ SparkAuto/Pages/ServiceTypes/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IList<ServiceType> ServiceType { get; set; }
+        public async Task<IActionResult> OnGet()
+        {
+            ServiceType = await _db.ServiceType.ToListAsync();
+            return Page();
+        }
+    }
+}
